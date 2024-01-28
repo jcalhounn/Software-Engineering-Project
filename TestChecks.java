265b2ec4adb6b56e316082219d09f834d1dece7b
Jordan Calhoun <152930109+jcalhounn@users.noreply.github.com>
2024-01-28 12:58:45 -0500

    Update and rename testChecks.java to TestChecks.java

---
+++ TestChecks.java
@@ -0,0 +1,8 @@
+public class TestChecks{
+  public static void main(String[] args){
+    boolean test = true;
+    if(test == true){
+      System.out.println("Hello");
+    }
+  }
+}
--- testChecks.java
+++
@@ -1,8 +0,0 @@
-public class testChecks{
-  public static void main(String[] args){
-      boolean test = true;
-      if(test == true){
-        System.out.println("Hello");
-        }
-    }
-}
