07d341e2386aef83a925fbfb22f952cd0671acc9
Jordan Calhoun <152930109+jcalhounn@users.noreply.github.com>
2024-01-28 12:54:06 -0500

    Update testChecks.java
    updated attempt

--- testChecks.java
+++ testChecks.java
@@ -1,11 +1,8 @@
 public class testChecks{
-    public static void main(String[] args)
-    {
-        boolean test = true;
-
-        if(test == true)
-        {
-            System.out.println("Hello");
+  public static void main(String[] args){
+      boolean test = true;
+      if(test == true){
+        System.out.println("Hello");
         }
     }
 }
