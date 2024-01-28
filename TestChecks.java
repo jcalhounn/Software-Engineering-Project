c067be475ed9bfef5d74ec66c38658a762db5d80
Jordan Calhoun <152930109+jcalhounn@users.noreply.github.com>
2024-01-28 13:10:13 -0500

    Updated New style example for changing checkstyle
    Desired New Style

--- TestChecks.java
+++ TestChecks.java
@@ -1,8 +1,12 @@
-public class TestChecks{
-  public static void main(String[] args){
-    boolean test = true;
-    if(test == true){
-      System.out.println("Hello");
+public class TestChecks
+{
+    public static void main(String[] args)
+    {
+        boolean test = true;
+
+        if(test == true)
+        {
+            System.out.println("Hello");
+        }
     }
-  }
 }
