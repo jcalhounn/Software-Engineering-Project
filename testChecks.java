e715dc94e0bdee40af91c56789153c4c2428df81
Jordan Calhoun <152930109+jcalhounn@users.noreply.github.com>
2024-01-28 12:46:12 -0500

    test file
    test checks

---
+++ testChecks.java
@@ -0,0 +1,9 @@
+public class testChecks{
+    public static void main(String[] args)
+    {
+        boolean test = true;
+
+        if(test == true)
+            System.out.println("Hello");
+    }
+}
