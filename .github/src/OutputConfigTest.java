c6813339621e6710cf27a2d912bcad2b2ac4f883
Jordan Calhoun <152930109+jcalhounn@users.noreply.github.com>
2024-02-12 16:55:56 -0500

    integration test implementation (#13)
    * integration test implementation
    
    * Update ComputeEngineIntegrationTest.java

---
+++ .github/src/OutputConfigTest.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class OutputConfigTest implements OutputConfig {
+
+    @Override
+    public void configureOutput(List<String> list) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'configureConfig'");
+    }
+    
+    
+}
