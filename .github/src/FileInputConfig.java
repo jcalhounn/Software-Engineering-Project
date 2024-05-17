9fe98b920918c2f01647e4103fd0aff79b67dc71
Jordan Calhoun <152930109+jcalhounn@users.noreply.github.com>
2024-05-16 21:12:03 -0400

    checkstyle fix

--- .github/src/FileInputConfig.java
+++ .github/src/FileInputConfig.java
@@ -4,11 +4,12 @@ public class FileInputConfig implements InputConfig{
     private final char delimiter;
 
     public FileInputConfig(String fileName, char delimiter){
-
         this.fileName  = fileName;
         this.delimiter = delimiter;
     }
-    public char getDelimiter() {return delimiter; }
+    public char getDelimiter() {
+        return delimiter;
+    }
 
     public String getFileName(){
         return fileName;
