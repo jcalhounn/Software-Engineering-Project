a8abfbdded1ffaee5f95014ec2cde1fd8644f308
Jordan Calhoun <152930109+jcalhounn@users.noreply.github.com>
2024-05-16 21:10:32 -0400

    checkstyle fixes again

--- .github/src/FileOutputConfig.java
+++ .github/src/FileOutputConfig.java
@@ -4,13 +4,11 @@ public class FileOutputConfig implements OutputConfig {
 	private final char delimiter;
 
 	public FileOutputConfig(String fileName, char delimiter) {
-
 		this.fileName = fileName;
 		this.delimiter = delimiter;
 	}
 
 	public String getFileName() {
-
 		return fileName;
 	}
 
